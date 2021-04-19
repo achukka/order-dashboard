1e95e8eaec8d137439cf223d1f5113102d2bd979
Aditya Chukka <adityaharish626@gmail.com>
2021-04-19 00:46:23 -0400

    Remove: Logos and png

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { link } from 'fs/promises';
 
-test('renders learn react link', () => {
+test('renders user name po', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello Po/i);  
   expect(linkElement).toBeInTheDocument();
 });
